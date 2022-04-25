83d5e17972ad2ba427a22dc5d5e1ed2f5c10c8e8
zs <zs@123390.163.com>
2022-04-25 10:14:44 +0800

    swagger 使用

--- yygh_parent/service/service_hosp/src/main/java/com/atguigu/yygh/ServiceHospitalApplication.java
+++ yygh_parent/service/service_hosp/src/main/java/com/atguigu/yygh/ServiceHospitalApplication.java
@@ -2,8 +2,10 @@ package com.atguigu.yygh;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.ComponentScan;
 
 @SpringBootApplication
+@ComponentScan(basePackages = "com.atguigu")
 public class ServiceHospitalApplication {
     public static void main(String[] args) {
         SpringApplication.run(ServiceHospitalApplication.class,args);
